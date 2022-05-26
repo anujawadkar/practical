3a6d665fccd00962db515c37d603f23a6826218f
anujawadkar <96774711+anujawadkar@users.noreply.github.com>
2022-05-26 12:42:57 +0530

    Add files via upload

---
+++ Hands-on/Assignment/StarPattern.java
@@ -0,0 +1,24 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class StarPattern
+{
+  public static void main(String args[])
+  {
+      int n;
+      Scanner sc = new Scanner(System.in);
+      n=sc.nextInt();
+      
+      for(int i=0;i<n;i++)
+      {
+         
+          for(int j=0;j<n;j++)
+          {
+             
+              System.out.print("*  ");
+          }
+          System.out.println();
+      }
+  }
+}
