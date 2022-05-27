fdcbb3c3ef8e9dd60ffc6ac3196df3ad944032ea
anujawadkar <96774711+anujawadkar@users.noreply.github.com>
2022-05-27 14:31:50 +0530

    Add files via upload

---
+++ Practical/Constructor/Default & No Arguement/MyDefaultConstructor.java
@@ -0,0 +1,17 @@
+package corejava;
+
+public class MyDefaultConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+			{
+				System.out.println("This Is Default Constructor/ No Arguement Constructor");
+			}
+					
+				MyDefaultConstructor mdc = new MyDefaultConstructor();
+				
+			}		
+	}
+
+
