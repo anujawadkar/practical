ec3c55f42c1f0aaebdc63491d9923d8689dbbe78
anujawadkar <96774711+anujawadkar@users.noreply.github.com>
2022-05-27 14:06:21 +0530

    Add files via upload

---
+++ Assignment/TestDriveFive.java
@@ -0,0 +1,20 @@
+package corejava;
+
+public class TestDriveFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] intArr = new int [5]; 
+		for (int i = 0; i < intArr . length ; i ++) 
+			intArr [ i ] = i * 2;
+		 
+		 for (int i = 0; i < intArr . length ; i ++) 
+		{ 
+		System . out . print ( intArr [ i ] + ", ") ;
+		 }
+		 } 			
+	{
+	}
+
+}
