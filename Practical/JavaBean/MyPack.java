422026df3d85c5f3377a128876517f6f4dfe9175
anujawadkar <96774711+anujawadkar@users.noreply.github.com>
2022-05-27 11:32:06 +0530

    Add files via upload

---
+++ Practical/JavaBean/MyPack.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class MyPack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		{
+	{
+		 employ e=new employ();
+		 //object is created
+		 e.setName("Anuja");
+		 //setting value to the object
+		 System.out.println(e.getName());
+		 }} 
+	}
+
+}
---
+++ Practical/JavaBean/employ.java
@@ -0,0 +1,16 @@
+package corejava;
+
+
+		
+		public class employ implements java.io.Serializable{
+	     private int id;
+		 private String name;
+		 public employ(){}
+		 public void setId(int id){this.id=id;}
+		 public int getId(){return id;}
+		 public void setName(String name){this.name=name;}
+		 public String getName(){return name;}
+
+	}
+
+
