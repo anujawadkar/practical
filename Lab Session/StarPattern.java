44aeec3aa5e49ed1a5a385f1b3546b77df8ce3d0
anujawadkar <96774711+anujawadkar@users.noreply.github.com>
2022-05-30 12:07:45 +0530

    Add files via upload

---
+++ Lab Session/StarPattern.java
@@ -0,0 +1,27 @@
+package corejava;
+// java program to print star pattern
+ 
+import java.util.Scanner;
+public class StarPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		{
+				      int n;
+		      Scanner sc = new Scanner(System.in);
+		      n=sc.nextInt();
+		      
+		      for(int i=0;i<4;i++)
+		      {
+		         
+		          for(int j=0;j<10;j++)
+		          {
+		             
+		              System.out.print("*  ");
+		          }
+		          System.out.println();
+		      }
+		  }
+		}
+	}
+
