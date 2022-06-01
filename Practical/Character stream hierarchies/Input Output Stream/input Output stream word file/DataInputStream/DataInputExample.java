c638680684d548125ea7bae33c3ff4df47cca7dd
anujawadkar <96774711+anujawadkar@users.noreply.github.com>
2022-06-01 14:01:20 +0530

    Add files via upload

---
+++ Practical/Character stream hierarchies/Input Output Stream/input Output stream word file/DataInputStream/DataInputExample.java
@@ -0,0 +1,19 @@
+package corejava;
+import java.io.*;  
+public class DataInputExample {
+			
+	  public static void main(String[] args) throws IOException {  
+		    InputStream input = new FileInputStream("D:\\myFile.txt");  
+		    DataInputStream inst = new DataInputStream(input);  
+		    int count = input.available();  
+		    byte[] ary = new byte[count];  
+		    inst.read(ary);  
+		    for (byte bt : ary) {  
+		     char k = (char) bt;  
+		      System.out.print(k+"-");  
+		    }  
+		  }  
+		
+	}
+
+
