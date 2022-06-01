dd95b25122c341c3d6aeee8a8034ee330e7b6c84
anujawadkar <96774711+anujawadkar@users.noreply.github.com>
2022-06-01 12:09:25 +0530

    Add files via upload

---
+++ Practical/Character stream hierarchies/Buffer Reader/BufferedReaderTwo.java
@@ -0,0 +1,24 @@
+package corejava;
+
+import java.io.*;  
+	
+public class BufferedReaderTwo{    
+	
+public static void main(String args[])throws Exception{             
+	
+	//Creating object
+    InputStreamReader r=new InputStreamReader(System.in);    
+	
+    //To read from the created file
+    BufferedReader br=new BufferedReader(r);            
+	
+    System.out.println("Enter your name");    
+	
+    //To Reading The Value From The User
+    String name=br.readLine();    
+	
+    System.out.println("Welcome "+name);    
+	
+	}    
+	
+} 
