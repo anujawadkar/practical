400e906b152756a77e3a59192b976f7720622079
anujawadkar <96774711+anujawadkar@users.noreply.github.com>
2022-06-07 14:09:48 +0530

    Add files via upload

---
+++ Practical/Maven Practical/world/src/main/java/com/hello/world/App.java
@@ -0,0 +1,16 @@
+//com.helloworld = Group Id
+//helloWorld= Artifact Id
+
+package com.hello.world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
